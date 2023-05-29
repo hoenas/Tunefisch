d694721c84a76f46e0096e3d646e923c8e55f57a
hoenas <hoenas@users.noreply.github.com>
2023-05-29 16:18:12 +0200

    Removed obsolete code

--- tunefish/tunefish.ino
+++ tunefish/tunefish.ino
@@ -5,7 +5,7 @@ arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
 /*
 These values can be changed in order to evaluate the functions
 */
-const uint16_t samples = 512; // This value MUST ALWAYS be a power of 2
+const uint16_t samples = 2048; // This value MUST ALWAYS be a power of 2
 const double signalFrequency = 1000;
 const double samplingFrequency = 5000;
 const uint8_t amplitude = 100;
@@ -39,16 +39,7 @@ void findPeaks()
 
 void loop()
 {
-
-  /* Build raw data */
-  double cycles = (((samples - 1) * signalFrequency) / samplingFrequency); // Number of signal cycles that the sampling will read
-  for (uint16_t i = 0; i < samples; i++)
-  {
-    vReal[i] = int8_t((amplitude * (sin((i * (twoPi * cycles)) / samples))) / 2.0); /* Build data with positive and negative values*/
-    // vReal[i] = uint8_t((amplitude * (sin((i * (twoPi * cycles)) / samples) + 1.0)) / 2.0);/* Build data displaced on the Y axis to include only positive values*/
-    vImag[i] = 0.0; // Imaginary part must be zeroed in case of looping to avoid wrong calculations and overflows
-  }
-
+  calculateFFT();
   double x = FFT.MajorPeak(vReal, samples, samplingFrequency);
   auto lookup = tones_lookup::get_nearest_tone(44.0);
   Serial.println(lookup.name);
