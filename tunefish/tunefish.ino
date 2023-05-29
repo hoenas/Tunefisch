5e57951a6e78cf9c424aae93eaffb96142df17e0
hoenas <hoenas@users.noreply.github.com>
2023-05-29 11:10:32 +0200

    Added initial sketch

---
+++ tunefish/tunefish.ino
@@ -0,0 +1,57 @@
+#include "arduinoFFT.h"
+#include "Arduino.h"
+
+arduinoFFT FFT = arduinoFFT(); /* Create FFT object */
+/*
+These values can be changed in order to evaluate the functions
+*/
+const uint16_t samples = 512; //This value MUST ALWAYS be a power of 2
+const double signalFrequency = 1000;
+const double samplingFrequency = 5000;
+const uint8_t amplitude = 100;
+/*
+These are the input and output vectors
+Input vectors receive computed results from FFT
+*/
+double vReal[samples];
+double vImag[samples];
+
+struct {
+  char name[3];
+  double frequency;
+} Tone;
+
+void setup()
+{
+  Serial.begin(115200);
+  while(!Serial);
+  Serial.println("Ready");
+}
+
+void calculateFFT() {
+  /* Print the results of the simulated sampling according to time */
+  FFT.Windowing(vReal, samples, FFT_WIN_TYP_HAMMING, FFT_FORWARD);	/* Weigh data */
+  FFT.Compute(vReal, vImag, samples, FFT_FORWARD); /* Compute FFT */
+  FFT.ComplexToMagnitude(vReal, vImag, samples); /* Compute magnitudes */
+}
+
+void findPeaks(){
+  FFT.MajorPeak();
+}
+
+void loop()
+{
+
+  /* Build raw data */
+  double cycles = (((samples-1) * signalFrequency) / samplingFrequency); //Number of signal cycles that the sampling will read
+  for (uint16_t i = 0; i < samples; i++)
+  {
+    vReal[i] = int8_t((amplitude * (sin((i * (twoPi * cycles)) / samples))) / 2.0);/* Build data with positive and negative values*/
+    //vReal[i] = uint8_t((amplitude * (sin((i * (twoPi * cycles)) / samples) + 1.0)) / 2.0);/* Build data displaced on the Y axis to include only positive values*/
+    vImag[i] = 0.0; //Imaginary part must be zeroed in case of looping to avoid wrong calculations and overflows
+  }
+  
+  double x = FFT.MajorPeak(vReal, samples, samplingFrequency);
+  Serial.println(x);
+}
+
